568672ac95946c4d3b1a345c912ba79fe30d96f8
Nazimaalmazovna <nazimaalmazovna419@gmail.com>
2023-10-12 04:32:57 +0300

    first js home work

---
+++ Task/main.js
@@ -0,0 +1,24 @@
+//? Задача №1
+
+const Name = 'Назима' 
+const Age = '19'
+const homeTown = 'Каракол'
+
+console.log(Name, Age, homeTown)
+
+
+//? Задача №2
+
+const fav_number = 97
+const fav_color = 'blue'
+
+console.log("Тип данных переменной fav_color: " + typeof fav_color)
+console.log("Тип данных переменной fav_number: " + typeof fav_number)
+
+
+//? Задача №3
+
+const firWord = 'Hello '
+const secWord = 'World!'
+
+console.log(firWord + secWord)
